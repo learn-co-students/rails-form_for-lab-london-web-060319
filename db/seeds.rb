305b929a9473180551ae17963c661d68478a88b3
Danielle <dmiz@brandeis.edu>
2019-06-29 07:38:50 +0100

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create( [
+    { first_name:"Henry", last_name:"Mika" },
+    { first_name:"Fred",last_name:"Guy"},
+    { first_name:"Barr", last_name:"Yaron"}
+])
+
+SchoolClass.create( [
+    { title:"Math", room_number:25 },
+    { title:"History", room_number:39 },
+    { title:"Science", room_number:103 }
+])
